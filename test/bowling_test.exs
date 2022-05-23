c912fd4ce5abf348b6b66c47c50954a5e2a76d35
Jacky Lee <jacky@crypto.com>
2022-05-24 07:33:06 +0800

    refactor duplicated loop fn into its own fn

--- test/bowling_test.exs
+++ test/bowling_test.exs
@@ -5,23 +5,19 @@ defmodule BowlingTest do
     [state: %Bowling{}]
   end
 
-  test "roll a gutter game", %{state: state} do
-    new_state =
-      Enum.reduce(1..20, state, fn _x, old_state ->
-        Bowling.roll(old_state, 0)
-      end)
+  def roll_remaining_rolls(state, pin) do
+    Enum.reduce(1..20, state, fn _x, old_state ->
+      Bowling.roll(old_state, pin)
+    end)
+  end
 
+  test "roll a gutter game", %{state: state} do
+    new_state = roll_remaining_rolls(state, 0)
     assert 0 == Bowling.score(new_state)
   end
 
   test "roll a game with only 1s", %{state: state} do
-    new_state =
-      Enum.reduce(1..20, state, fn _x, old_state ->
-        Bowling.roll(old_state, 1)
-      end)
-
+    new_state = roll_remaining_rolls(state, 1)
     assert 20 == Bowling.score(new_state)
   end
-
-
 end
